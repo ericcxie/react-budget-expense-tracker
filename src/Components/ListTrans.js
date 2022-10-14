e26e9d691f0259a5fcb169125fa56dd78ce79f26
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2022-10-13 22:07:22 -0400

    Update ListTrans.js

--- src/Components/ListTrans.js
+++ src/Components/ListTrans.js
@@ -7,7 +7,7 @@ export const List_trans = () => {
 
   return (
     <div>
-        <h3>Activity History</h3>
+        <h3>Transaction History</h3>
       <ul className="list">
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
