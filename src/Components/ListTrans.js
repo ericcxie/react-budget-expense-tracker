d127785cd7c1c6d11da71884e93f0315be1b8eb1
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2022-10-14 01:26:29 -0400

    Update ListTrans.js

--- src/Components/ListTrans.js
+++ src/Components/ListTrans.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../Context/GlobalState';
 import { Transaction } from './Transaction';
 
-export const List_trans = () => {
+export const ListTrans = () => {
   const { transactions } = useContext(GlobalContext);
 
   return (
     <div>
-        <h3>Transaction History</h3>
+        <strong>
+          <h3>Transaction History</h3>
+        </strong>
       <ul className="list">
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
